206b03aea7167132002966e687044aaf7719d10f
Kiran Challa <kichalla@microsoft.com>
2015-02-10 13:00:38 -0800

    Fixed XmlAssert and added tests

--- test/Microsoft.AspNet.Mvc.Xml.Test/XmlAssert.cs
+++ test/Microsoft.AspNet.Mvc.Xml.Test/XmlAssert.cs
@@ -2,7 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.IO;
 using System.Linq;
+using System.Xml;
 using System.Xml.Linq;
 using Xunit.Sdk;
 
@@ -20,30 +22,62 @@ namespace Microsoft.AspNet.Mvc.Xml
         /// <param name="actualXml">Actual xml string.</param>
         public static void Equal(string expectedXml, string actualXml)
         {
-            var sortedExpectedXDoc = SortAttributes(XDocument.Parse(expectedXml));
-            var sortedActualXDoc = SortAttributes(XDocument.Parse(actualXml));
+			var sortedExpectedXDoc = XDocument.Parse(expectedXml).SortAttributes();
+			var sortedActualXDoc = XDocument.Parse(actualXml).SortAttributes();
 
-            bool areEqual = XNode.DeepEquals(sortedExpectedXDoc, sortedActualXDoc);
+			// Since XNode's DeepEquals does not check for presence of xml declaration,
+			// check it explicitly
+			bool areEqual = VerifyXmlDeclaration(sortedExpectedXDoc.Declaration, sortedActualXDoc.Declaration);
+			areEqual = areEqual && XNode.DeepEquals(sortedExpectedXDoc, sortedActualXDoc);
 
             if (!areEqual)
             {
-                throw new EqualException(sortedExpectedXDoc, sortedActualXDoc);
+				throw new EqualException(sortedExpectedXDoc.GetRawXml(), sortedActualXDoc.GetRawXml());
             }
         }
 
-        private static XDocument SortAttributes(XDocument doc)
+		private static bool VerifyXmlDeclaration(XDeclaration expected, XDeclaration actual)
+		{
+			if (expected == null && actual == null)
+			{
+				return true;
+			}
+
+			if (expected == null || actual == null)
+			{
+				return false;
+			}
+
+			return string.Equals(expected.Version, actual.Version, StringComparison.OrdinalIgnoreCase)
+				&& string.Equals(expected.Encoding, actual.Encoding, StringComparison.OrdinalIgnoreCase);
+		}
+		private static XDocument SortAttributes(this XDocument doc)
         {
             return new XDocument(
                     doc.Declaration,
-                    SortAttributes(doc.Root));
+					doc.Nodes().OfType<XText>().FirstOrDefault(),
+					doc.Root.SortAttributes());
         }
 
-        private static XElement SortAttributes(XElement element)
+		private static XElement SortAttributes(this XElement element)
         {
             return new XElement(
                     element.Name,
+					element.Nodes().OfType<XText>().FirstOrDefault(),
                     element.Attributes().OrderBy(a => a.Name.ToString()),
-                    element.Elements().Select(child => SortAttributes(child)));
+					element.Elements().Select(child => child.SortAttributes()));
         }
+		private static string GetRawXml(this XDocument xdocument)
+		{
+			string xml = null;
+			var stream = new MemoryStream();
+			xdocument.Save(stream, SaveOptions.DisableFormatting);
+			stream.Position = 0;
+			using (var reader = new StreamReader(stream))
+			{
+				xml = reader.ReadToEnd();
     }
+			return xml;
+		}
+	}
 }
---
+++ test/Microsoft.AspNet.Mvc.Xml.Test/XmlAssertTest.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Xunit;
+using Xunit.Sdk;
+
+namespace Microsoft.AspNet.Mvc.Xml.Test
+{
+	public class XmlAssertTest
+	{
+		[Theory]
+		[InlineData("<A>hello</A>", "<A>hey</A>")]
+		[InlineData("<A><B>hello world</B></A>", "<A><B>hello world!!</B></A>")]
+		public void ValidatesTextNodes(string input1, string input2)
+		{
+			Assert.Throws<EqualException>(() => XmlAssert.Equal(input1, input2));
+		}
+
+		[Theory]
+		[InlineData("<A></A>", "<A></A>")]
+		[InlineData("<A/>", "<A/>")]
+		public void DoesNotFail_OnEmptyTextNodes(string input1, string input2)
+		{
+			XmlAssert.Equal(input1, input2);
+		}
+
+		[Theory]
+		[InlineData("<?xml version=\"1.0\" encoding=\"UTF-8\"?><A></A>", 
+			"<A></A>")]
+		[InlineData("<?xml version=\"1.0\" encoding=\"UTF-8\"?><A></A>", 
+			"<?xml version=\"1.0\" encoding=\"UTF-16\"?><A></A>")]
+		public void Validates_XmlDeclaration(string input1, string input2)
+		{
+			Assert.Throws<EqualException>(() => XmlAssert.Equal(input1, input2));
+		}
+
+		[Fact]
+		public void Validates_XmlDeclaration_IgnoresCase()
+		{
+			// Arrange and Act
+			var input1 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+						"<A><B color=\"red\" size=\"medium\">hello world</B></A>";
+			var input2 = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+						"<A><B size=\"medium\" color=\"red\">hello world</B></A>";
+
+			// Assert
+			XmlAssert.Equal(input1, input2);
+		}
+
+		[Fact]
+		public void IgnoresAttributeOrder()
+		{
+			// Arrange and Act
+			var input1 = "<A><B color=\"red\" size=\"medium\">hello world</B></A>";
+			var input2 = "<A><B size=\"medium\" color=\"red\">hello world</B></A>";
+
+			// Assert
+			XmlAssert.Equal(input1, input2);
+		}
+
+		[Fact]
+		public void ValidatesAttributes_IgnoringOrder()
+		{
+			// Arrange and Act
+			var input1 = "<A><B color=\"red\" size=\"medium\">hello world</B></A>";
+			var input2 = "<A><B size=\"Medium\" color=\"red\">hello world</B></A>";
+
+			// Assert
+			Assert.Throws<EqualException>(() => XmlAssert.Equal(input1, input2));
+		}
+	}
+}

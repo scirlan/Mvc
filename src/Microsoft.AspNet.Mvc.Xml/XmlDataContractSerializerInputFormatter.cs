ac0cc5470f85e9101e308a21e89b7125539da371
Kiran Challa <kichalla@github.com>
2015-02-12 06:44:40 -0800

    Added support for validation of Required attributes in case of XmlDataContractSerializerInputFormatter

--- src/Microsoft.AspNet.Mvc.Xml/XmlDataContractSerializerInputFormatter.cs
+++ src/Microsoft.AspNet.Mvc.Xml/XmlDataContractSerializerInputFormatter.cs
@@ -91,6 +91,8 @@ namespace Microsoft.AspNet.Mvc.Xml
 
             using (var xmlReader = CreateXmlReader(new NonDisposableStream(request.Body)))
             {
+                RequiredValidationHelper.Validate(context.ModelType, context.ActionContext.ModelState);
+
                 var type = GetSerializableType(context.ModelType);
 
                 var serializer = CreateSerializer(type);
